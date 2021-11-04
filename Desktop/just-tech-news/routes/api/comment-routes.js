12522cad1b9158335dbc2249c4a024cae5b300e0
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-04 14:20:36 -0700

    routes updated to include comments

--- Desktop/just-tech-news/routes/api/comment-routes.js
+++ Desktop/just-tech-news/routes/api/comment-routes.js
@@ -1,9 +1,30 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-router.get('/', (req, res) => {
-
-});
+// router.get('/', (req, res) => {
+//     console.log('======================');
+//     Comment.findAll({
+//       attributes: [
+//         'id',
+//         'comment_text',
+//         'title',
+//         'created_at',
+//         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+//       ],
+//       order: [['created_at', 'DESC']],
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['username']
+//         }
+//       ]
+//     })
+//       .then(dbPostData => res.json(dbPostData))
+//       .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//       });
+// });
 
 router.post('/', (req, res) => {
     Comment.create({
@@ -20,7 +41,22 @@ router.post('/', (req, res) => {
 
 
 router.delete('/:id', (req, res) => {
-
+    Comment.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbPostData => {
+          if (!dbPostData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+          }
+          res.json(dbPostData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
 });
 
 module.exports = router;
